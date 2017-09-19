f3a1ce84a9482bc2e8ca442f11caa5bcc4d40006
jeferson.passos <jeferson.passos@portoseguro.com.br>
2017-09-19 14:57:07 -0300

    add log in routes

--- src/routes/ticket.route.php
+++ src/routes/ticket.route.php
@@ -7,10 +7,11 @@ require_once __DIR__.'/../controllers/ticket.controller.php';
 $ticket = new Ticket($app);
 
 $app->post('/ticket', function () use($ticket, $app){
+    $app->log->info(" GET '/ticket' route");
     $ticket->newTicket();
 });
 
-$app->get('/tickets', function () use($ticket, $app){
-    $app->log->info("Slim-Skeleton '/ticket' route");
+$app->get('/ticket', function () use($ticket, $app){
+    $app->log->info("GET '/tickets' route");
     $ticket->getTickets();
 });
--- src/routes/user.route.php
+++ src/routes/user.route.php
@@ -5,21 +5,25 @@ require_once __DIR__.'/../controllers/user.controller.php';
 
 $user = new User($app);
 
-$app->get('/users', function() use ($user, $app){
+$app->get('/user', function() use ($user, $app){
+    $app->log->info(" GET '/user' route");
     $user->getUsers();
 });
 
 $app->get('/user/:id', function ($id) use($user, $app){
+    $app->log->info("GET '/ticket/:id' route");
     $user->getUser($id);
 });
 
 $app->post('/user', function() use ($user, $app){
+    $app->log->info(" POST '/ticket' route");
     $data = $app->request->getBody();
     $data = json_decode($data, true);
     $user->registerUser($data);
 });
 
-$app->post('/user/auth', function () use ($user, $app){
+$app->post('/auth', function () use ($user, $app){
+    $app->log->info(" POST '/auth' route");
     $data = $app->request->getBody();
     $data = json_decode($data);
     $user->auth($data->user, $data->pass);
